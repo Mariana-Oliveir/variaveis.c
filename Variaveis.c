10e93ab8002d519f1660ee09c9c43e9b30540b19
Mariana-Oliveir <82880003+Mariana-Oliveir@users.noreply.github.com>
2025-03-04 19:30:06 -0300

    Create Variaveis.c

---
+++ Variaveis.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+int idade =50;
+int quantidade = 1;
+float altura = 1.75;
+double peso = 90.3;
+char opçao = 'A';
+char nome[20] = "Mariana";
+
+printf("A Idade de %s é: %d\n", nome , idade);
+    printf("A Altura é : %.2f metros\n", altura);
+    printf("A letra é : %c\n", opçao);
+
+    return 0;
+
+
+}
